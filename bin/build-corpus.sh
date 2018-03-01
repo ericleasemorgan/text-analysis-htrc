f89fd67b71cda54bc13de94b1517e3d3aa306dc1
Eric Lease Morgan <eric_morgan@infomotions.com>
2018-03-01 11:27:05 -0500

    Cleaned up a bit.

--- bin/build-corpus.sh
+++ bin/build-corpus.sh
@@ -3,9 +3,10 @@
 # build-corpus.sh - given an rsync shell script and a keyword, cache HathiTrust content and make it browsable
 
 # Eric Lease Morgan <emorgan@nd.edu>
-# May 21, 2015 - first investigations
-# May 31, 2015 - added charting (oops!) as well as pretty about page; fifteen years ago today, Dad died
-# June 2, 2015 - added sanity checking; removed need for rename command
+# May 21, 2015  - first investigations
+# May 31, 2015  - added charting (oops!) as well as pretty about page; fifteen years ago today, Dad died
+# June 2, 2015  - added sanity checking; removed need for rename command
+# March 1, 2018 - started adding with earnest the ability to put into HTRC data capsule
 
 
 # get input
@@ -31,25 +32,18 @@ mkdir ./collections/$NAME/graphs
 
 # harvest the content
 chmod +x $RSYNCSCRIPT
-#cp $RSYNCSCRIPT $NAME/json
-#cd $NAME/json
-#RSYNCSCRIPT=$( basename $RSYNCSCRIPT )
-#./$RSYNCSCRIPT
 cd ./collections/$NAME/bz
 ../../../$RSYNCSCRIPT
 cd ../../..
 
-# get rid of "advanced" files, uncompress everything, and rename the balance
-#rm *.advanced.*
+# uncompress everything and remove unneeded files
 find ./collections/$NAME/bz -name "*.bz2" -exec cp {} ./collections/$NAME/json \;
 bunzip2 ./collections/$NAME/json/*.bz2
 rm ./collections/$NAME/json/*.bz2
 
-# stage #2 - create the index
-#cd ../..
-#./bin/make-index.sh $NAME
-find ./collections/$NAME/json -name '*.json' | parallel ./bin/make-index.sh $NAME {}
 
+# stage #2 - create the index
+find ./collections/$NAME/json -name '*.json' | parallel ./bin/make-index.sh $NAME {}
 
 # make dictionary
 ./bin/make-dictionary.py ./collections/$NAME/index/ > ./collections/$NAME/dictionary.db
@@ -60,6 +54,7 @@ cat ./collections/$NAME/dictionary.db | ./bin/list-unique.py  > ./collections/$N
 
 # stage #3 - create the catalog
 ./bin/make-catalog.sh $NAME
+
 
 # stage #4 - create sorted numeric reports
 ./bin/calculate-size.sh   ./collections/$NAME                      | sort -k2 -n -r > ./collections/$NAME/sizes.db
@@ -72,8 +67,9 @@ cat ./collections/$NAME/dictionary.db | ./bin/list-unique.py  > ./collections/$N
 ./bin/calculate-themes.py -v ./collections/$NAME/dictionary.db etc/theme-names.txt  > ./collections/$NAME/dictionary-names.db
 ./bin/calculate-themes.py -v ./collections/$NAME/dictionary.db etc/theme-ideas.txt  > ./collections/$NAME/dictionary-ideas.db
 
-# create charts; R needs to be installed (oops!); commented out so people don't need R, yet
+# create charts; R needs to be installed
 ./bin/make-graphs.sh ./collections/$NAME
+
 
 # state 5 - analyze corpus and create pretty about page
 ./bin/about.sh ./collections/$NAME > ./collections/$NAME/about.db
